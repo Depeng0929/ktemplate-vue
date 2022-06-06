e9a4611f622ff43d15afc5a2d319978a2731c54d
depeng.kong <15365182986@163.com>
2022-06-06 09:32:47 +0800

    style: add dark theme  adapter

--- unocss.config.ts
+++ unocss.config.ts
@@ -10,6 +10,10 @@ import {
 export default defineConfig({
   shortcuts: [
     ['btn', 'px-4 py-1 rounded inline-block text-white cursor-pointer tracking-wide opacity-90 hover:opacity-100 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50 disabled:pointer-events-none'],
+    ['border-base', 'border-gray-200 dark:border-dark-200'],
+    ['bg-base', 'bg-white dark:bg-dark-100'],
+    ['color-base', 'text-gray-900 dark:text-gray-300'],
+    ['color-fade', 'text-gray-900:50 dark:text-gray-300:50'],
   ],
   presets: [
     presetUno(),
